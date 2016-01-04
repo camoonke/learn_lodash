a053adbced1a7378be974b37bb74f12ad3ec1786
alsotang <alsotang@gmail.com>
2016-01-04 20:32:15 +0800

    _.compact

--- test/lib/lodash_array.test.js
+++ test/lib/lodash_array.test.js
@@ -1,11 +1,18 @@
-var lodash = require('../../lib/lodash_array');
+var _ = require('../../');
 
 describe('test/lodash.test.js', function () {
   describe('Arrays', function () {
     describe('#chunk', function () {
       it('should split the Array into chunks', function () {
         var arr = ['a', 'b', 'c', 'd', 'e', 'f']
-        lodash.chunk(arr, 2).should.eql([['a', 'b'], ['c', 'd'], ['e', 'f']])
+        _.chunk(arr, 2).should.eql([['a', 'b'], ['c', 'd'], ['e', 'f']])
+      })
+    })
+
+    describe('#compact', function () {
+      it('should filter all falsey values', function () {
+        _.compact([0, 1, 2, false, null, 0, '', "", void 0, NaN])
+          .should.eql([1, 2])
       })
     })
   })
