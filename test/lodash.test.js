159e674040ac0395440a02cab222a6824352f85b
camoonke <nhjjzcj@gmail.com>
2016-01-04 17:47:14 +0800

    格式缩进问题 node 2空格

--- test/lodash.test.js
+++ test/lodash.test.js
@@ -2,12 +2,12 @@ var lodash = require('../lib/lodash');
 var should = require('should')
 
 describe('test/lodash.test.js', function () {
-	describe('Arrays', function () {
-		describe('#chunk', function () {
-			it('should split the Array into chunks', function () {
-			  	var arr = ['a', 'b', 'c', 'd', 'e', 'f']
-			    lodash.chunk(arr, 2).should.eql([['a', 'b'], ['c', 'd'], ['e', 'f']])
-			})
-		})
-	})
+  describe('Arrays', function () {
+    describe('#chunk', function () {
+      it('should split the Array into chunks', function () {
+        var arr = ['a', 'b', 'c', 'd', 'e', 'f']
+        lodash.chunk(arr, 2).should.eql([['a', 'b'], ['c', 'd'], ['e', 'f']])
+      })
+    })
+  })
 })
