702ac197121c9929dc3154d5817dade53f947f0f
camoonke <nhjjzcj@gmail.com>
2016-01-05 21:23:55 +0800

    _difference,请吐槽一下

--- test/lib/lodash_array.test.js
+++ test/lib/lodash_array.test.js
@@ -3,16 +3,38 @@ var _ = require('../../');
 describe('test/lodash_array.test.js', function () {
   describe('Arrays', function () {
     describe('#chunk', function () {
-      it('should split the Array into chunks', function () {
+      it('_.chunk(arr, 2)', function () {
         var arr = ['a', 'b', 'c', 'd', 'e', 'f']
         _.chunk(arr, 2).should.eql([['a', 'b'], ['c', 'd'], ['e', 'f']])
       })
     })
 
     describe('#compact', function () {
-      it('should filter all falsey values', function () {
+      it('_.compact([0, 1, 2, false, null, 0, \'\', "", void 0, NaN])', function () {
         _.compact([0, 1, 2, false, null, 0, '', "", void 0, NaN])
           .should.eql([1, 2])
+      })
+    })
+
+    describe('#difference', function () {
+      it('_.difference()', function () {
+        _.difference()
+          .should.eql([])
+      })
+
+      it('_.difference([1, 2, 3])', function () {
+        _.difference([1, 2, 3])
+          .should.eql([1, 2, 3])
+      })
+
+      it('_.difference([1, 2, 3], [4, 2], [4, 1])', function () {
+        _.difference([1, 2, 3], [4, 2], [4, 1])
+          .should.eql([3])
+      })
+
+      it('_.difference([1, 2, 3], [4, 2])', function () {
+        _.difference([1, 2, 3], [4, 2])
+          .should.eql([1, 3])
       })
     })
   })
