ade45cf8822b8d78e1d10ac21e00151b5efbf328
alsotang <alsotang@gmail.com>
2016-01-05 21:10:33 +0800

    _.ary

--- test/lib/lodash_function.test.js
+++ test/lib/lodash_function.test.js
@@ -22,4 +22,15 @@ describe('test/lib/lodash_function.test.js', function () {
 
     count.should.eql(2)
   })
+
+  it('#ary', function () {
+    ['6', '8', '10'].map(_.ary(parseInt, 1))
+      .should.eql([6, 8, 10]);
+
+    ['6', '8', '10'].map(_.ary(parseInt, 2))
+      .should.eql([6, NaN, 2]);
+
+    ['6', '8', '10'].map(_.ary(parseInt))
+      .should.eql([6, NaN, 2]);
+  })
 })
